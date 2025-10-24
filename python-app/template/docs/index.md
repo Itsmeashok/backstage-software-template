edb99ac5b6605501006deae6f5c9d661302cf818
Itsmeashok <athapach@emeal.nttdata.com>
2025-10-24 12:18:33 +0200

    -

---
+++ python-app/template/docs/index.md
@@ -0,0 +1,12 @@
+# Documents for ${{values-app_name}}
+
+This Application has two endpoints:
+- `/api/v1/info`
+- `/api/v1/healthz`
+
+Here you could expand on what each of these endpoints do.
+
+
+# How to access the app?
+
+You can access the app by this following URL:  **p${{values-app_name}}-${{values-app_env}}.test.com/api/v1/info**
---
+++ python-app/template/src/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+
+def details():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S %p  on %B %d, %Y"),
+        'hostname':socket.gethostname(),
+        'message': 'Hello Humans, this is a test!',
+        'deployed_on':'Kubernetes!',
+        'application_name': '${{values.app_name}}',
+        'application_environment': '${{values.app_env}}'
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({ 
+        'status':'up',
+        'message': 'Hello, World!'
+        }), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
+
+# '/api/v1/details'
+# '/api/v1/healthz'
